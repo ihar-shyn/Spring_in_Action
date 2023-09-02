1129e45caee4021d8a9b382b455e6335b0a6cd3d
Igor Shinkevich <shinkevich@aras.com>
2023-09-02 17:40:33 +0200

    Init commit

---
+++ src/test/java/ihar/shyn/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ihar.shyn.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
